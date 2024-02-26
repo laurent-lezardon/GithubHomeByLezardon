9fa74ed13025862a45928de13863a3cf86cd3378
laurent-lezardon <59555017+laurent-lezardon@users.noreply.github.com>
2024-02-26 16:58:21 +0100

    "Add Project Pomodoro

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 export default function Cards() {
   return (
-    <div className="mx-auto max-w-[800px] pt-10 pb-20 grid gap-4 grid-cols-1 sm:grid-cols-2   2xl:grid-cols-3 ">
+    <div className="mx-auto max-w-[800px] pt-10 pb-20 grid gap-4 grid-cols-1 sm:grid-cols-2 2xl:max-w-[1200px]   2xl:grid-cols-3 ">
       <Card />
     </div>
   );
